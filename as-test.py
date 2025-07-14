d29aa877dc85475f778ccf1f10271b6476c0dfa5
Keyhole-Koro <korokororinthird@gmail.com>
2025-07-14 13:38:18 +0900

    simple for test code

--- as-test.py
+++ as-test.py
@@ -9,9 +9,8 @@ os.makedirs(INPUT_DIR, exist_ok=True)
 os.makedirs(OUTPUT_DIR, exist_ok=True)
 
 testcases = [
-    #("simpleFunc.c", "simpleFunc.masm", "simpleFunc.bin"),
-    #("simpleCondition.c", "simpleCondition.masm", "simpleCondition.bin"),
-    ("simpleFor.c", "simpleFor.masm", "simpleFor.bin"),
+    #("simpleFunc.masm", "simpleFunc.bin"),
+    ("simpleCondition.masm", "simpleCondition.bin"),
 ]
 
 def rel_to_subproj(path):
@@ -32,22 +31,17 @@ def run_step(cmd, stepname):
             print(f"  STDERR:\n{e.stderr}")
         sys.exit(1)
 
-for c_src, asm_out, bin_out in testcases:
-    c_path = os.path.join(INPUT_DIR, c_src)
-    asm_path = os.path.join(OUTPUT_DIR, asm_out)
+for asm_out, bin_out in testcases:
+    asm_path = os.path.join(INPUT_DIR, asm_out)
     bin_path = os.path.join(OUTPUT_DIR, bin_out)
 
-    c_path_rel = rel_to_subproj(c_path)
     asm_path_rel = rel_to_subproj(asm_path)
     bin_path_rel = rel_to_subproj(bin_path)
-
-    run_step(["make", "-C", "../MyCC", "clean"], "MyCC clean")
-    run_step(["make", "-C", "../MyCC", "run-mycc", f"IN={c_path_rel}", f"OUT={asm_path_rel}"], "C to ASM")
 
     run_step(["make", "-C", "../MyAssembler", "clean"], "MyAssembler clean")
     run_step(["make", "-C", "../MyAssembler", "run-myas", f"IN={asm_path_rel}", f"OUT={bin_path_rel}"], "ASM to BIN")
 
-    #run_step(["make", "-C", "../MyEmulator", "clean"], "MyEmulator clean")
+    run_step(["make", "-C", "../MyEmulator", "clean"], "MyEmulator clean")
     run_step(["make", "-C", "../MyEmulator", "run-myemu", f"IN={bin_path_rel}"], "BIN to Emulate")
 
 print("\nAll tests passed🎉")
--- inputs/simpleFunc.c
+++ inputs/simpleFunc.c
@@ -1,20 +1,14 @@
-int add(int a, int b) {
-    return a + b;
-}
 
 int add3(int a, int b, int c) {
     return a + b + c;
 }
 
-int add4(int a, int b, int c, int d) {
-    return a + b + c + d;
-}
-
 int main() {
     int x = 2;
     int y = 3;
-    int z = add(x, y);
-    z = add3(x, y, z);
-    z = add4(x, y, z, 10);
+    if (x > 9) {
+        x = x + 1; // x becomes 3
+    }
+    int z = add3(x, y, 10); // 15
     return z;
 }
